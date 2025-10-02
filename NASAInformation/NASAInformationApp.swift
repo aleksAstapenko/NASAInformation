6da08d46843d2b5bd3cef03f31f6373b73311087
aleksAstapenko <114617454+aleksAstapenko@users.noreply.github.com>
2025-10-02 18:58:31 +0300

    initial commit
    initial commit

---
+++ NASAInformation/NASAInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASAInformationApp.swift
+//  NASAInformation
+//
+//  Created by Александр Астапенко on 25.09.25.
+//
+
+import SwiftUI
+
+@main
+struct NASAInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
